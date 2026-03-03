7b4fa2e20d35546d414f97bd1386953034c9f911
ahmeddw1 <adventurecomputer1738@gmail.com>
2026-03-03 13:33:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ async def on_ready():
     print(f"Logged in as {bot.user}")
     await bot.change_presence(
         status=discord.Status.dnd,
-        activity=discord.CustomActivity(
-            name="🚀 Running On Best!"
+        discord.Activity(type=discord.ActivityType.watching, name="Tiktok")(
+            name="Tiktok"
         )
     )
 
